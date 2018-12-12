7d1a5ca5c696566a1eb04ba5ca062d3c2977306f
Jan Hellar <jhellar@redhat.com>
2018-12-12 10:57:57 +0100

    com1

--- e2e_tests/test.js
+++ e2e_tests/test.js
@@ -1,35 +1,72 @@
 const puppeteer = require('puppeteer');
+const assert = require('assert');
+
+const headless = process.env.HEADLESS !== 'false';
 
 let browser;
 let page;
 
-before('initialize puppeteer', async function() {
-  this.timeout(0);
+const initTest = context => {
+  context.timeout(0);
 
-  browser = await puppeteer.launch({ ignoreHTTPSErrors: true });
-  page = await browser.newPage();
-});
+  before('initialize puppeteer', async function() {
+    browser = await puppeteer.launch({ ignoreHTTPSErrors: true, headless });
+    page = await browser.newPage();
+  });
+  
+  after('close browser', async function() {
+    await browser.close();
+  });
+};
 
-after('close browser', async function() {
-  this.timeout(0);
-
-  await browser.close();
-});
+const login = async () => {
+  await page.goto(`https://${process.env.MDC_URL}`);
+  await Promise.all([
+    page.waitForNavigation(),
+    page.click('button[type=submit]')
+  ]);
+  await page.type('#inputUsername', 'developer');
+  await page.type('#inputPassword', 'developer');
+  await Promise.all([
+    page.waitForNavigation(),
+    page.click('button[type=submit]')
+  ]);
+  if (await page.$('input[name=approve]')) {
+    await Promise.all([
+      page.waitForNavigation(),
+      await page.click('input[name=approve]')
+    ]);
+  }
+};
 
 describe('login', function() {
-  this.timeout(0);
+  initTest(this);
 
   it('should log in', async function() {
-    await page.goto(process.env.MDC_URL);
-    await Promise.all([
-      page.waitForNavigation(),
-      page.click('button[type=submit]')
-    ]);
-    await page.type('#inputUsername', 'developer');
-    await page.type('#inputPassword', 'developer');
-    await Promise.all([
-      page.waitForNavigation(),
-      page.click('button[type=submit]')
-    ]);
+    await login();
   });
 });
+
+// describe('create app', function() {
+//   initTest(this);
+
+//   before('login', async function() {
+//     await login();
+//   });
+
+//   it('should create app', async function() {
+//     await page.click('.toolbar-pf button');
+
+//     await page.type('#name', 'test');
+//     await page.click('.modal-footer .btn-primary');
+
+//     await page.waitFor('.card-pf h1');
+//     const appTitleElement = page.$('.card-pf h1');
+//     const appTitle = await page.evaluate()
+
+//     const error = await page.$('.pficon-error-circle-o');
+//     assert.strictEqual(error, undefined, 'there should be no error when creating app');
+
+//     await new Promise(resolve => setTimeout(resolve, 20000));
+//   });
+// });
